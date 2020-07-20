4b5638507687cd9bbf708b387a535587380f7e21
Bmla9 <Bmla9@users.noreply.github.com>
2020-07-20 15:33:37 +0200

    Instantiating cubes

---
+++ unity boss fight/Assets/spawnScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnScript : MonoBehaviour
+{
+    public GameObject cube;
+    int timer = 0;
+
+    private void Update()
+    {
+        if (timer >= 100)
+        {
+            Instantiate(cube, transform);
+            timer = 0;
+        }
+        timer++;
+    }
+}
